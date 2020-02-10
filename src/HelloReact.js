c6ea0586ac7056724056caacd90a2a3664a7fc6b
Frank17008 <405267330@qq.com>
2020-02-11 00:51:48 +0800

    【React】初体验 HelloReact.js

---
+++ src/HelloReact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class HelloReact extends Component{
+    render(){
+        return(
+            <div>
+                Hello React!
+            </div>
+        )
+    }
+}
+
+export default HelloReact;
