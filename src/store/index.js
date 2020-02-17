07b7d490f0169ee414c3fb14ebd8b536a7a1bc1e
Frank17008 <405267330@qq.com>
2020-02-17 21:20:05 +0800

    【React】redux-thunk插件安装配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-/** @format */
+/** @format
+ * 2-17 20:00 redux-thunk中间件引入及配置
+ */
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
+import thunk from "redux-thunk";
+
+// 因为用到了redux devtools插件，因此使用compose 增强函数
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+const enhancers = composeEnhancers(applyMiddleware(thunk));
+
 const store = createStore(
     reducer,
-    // 如果插件存在则开启redux dev tools 插件 (需安装redux devtools插件)
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // 如果插件存在则开启redux devtools 插件 (需安装redux devtools插件)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancers
 );
 export default store;
