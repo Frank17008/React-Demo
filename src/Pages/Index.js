93a6da73fd9c5137c43e50881b2ea1f0103efa11
Frank17008 <405267330@qq.com>
2020-02-20 16:38:03 +0800

    【React-Router】路由传参跳转

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,12 +1,42 @@
 /** @format */
 
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {
+                    id: "p1",
+                    text: "这是段落1"
+                },
+                {
+                    id: "p2",
+                    text: "这是段落2"
+                },
+                {
+                    id: "p3",
+                    text: "这是段落3"
+                }
+            ]
+        };
+    }
     render() {
         return (
             <div>
                 <h2>Home Page</h2>
+                <ul>
+                    {this.state.list.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to={`/list/:${item.id}`}>
+                                    {item.text}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
