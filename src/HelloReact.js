ff700aaa14c2ec07cad9d787a665c3607d4cece1
Frank17008 <405267330@qq.com>
2020-02-22 23:44:44 +0800

    组件生命周期优化

--- src/HelloReact.js
+++ src/HelloReact.js
@@ -1,10 +1,31 @@
 /** @format */
 
 import React, { Component } from "react";
+import LifeCycles from "./LifeCycles";
 
 class HelloReact extends Component {
+    state = {
+        value: "1"
+    };
+    componentDidMount() {
+        this.setState({
+            value: "2"
+        });
+        setTimeout(() => {
+            this.setState({
+                value: ""
+            });
+        }, 2000);
+    }
     render() {
-        return <div>Hello React!</div>;
+        return (
+            <div>
+                Hello React!
+                {this.state.value && (
+                    <LifeCycles prop={this.state.value}></LifeCycles>
+                )}
+            </div>
+        );
     }
 }
 
--- src/LifeCycles.js
+++ src/LifeCycles.js
@@ -1,44 +1,45 @@
 /**
  * @format
- * @React生命周期
- *
+ * @React生命周期的触发条件
+ * 组件自身state状态发生改变
+ * 父组件传递的属性值发生改变时
  */
 
 import React, { Component } from "react";
 export default class LifeCycles extends Component {
     constructor(props) {
+        // 状态初始化
         super(props);
         this.state = {
             inputValue: ""
         };
     }
-
-    UNSAFE_componentWillMount() {
+    componentWillMount() {
         console.log("1--componentWillMount----组件将要挂载到页面");
     }
     componentDidMount() {
         console.log("3--componentDidMount---组件挂载完成");
     }
     shouldComponentUpdate() {
-        console.log("4--shouldComponentUpdate----组件是否要更新");
+        console.log("5--shouldComponentUpdate----组件是否要更新");
         return true;
     }
-    UNSAFE_componentWillUpdate() {
-        console.log("5--componentWillUpdate----组件将要更新");
+    componentWillUpdate() {
+        console.log("6--componentWillUpdate----组件将要更新");
     }
-    UNSAFE_componentDidUpdate() {
-        console.log("6--componentWillUpdate----组件更新完成");
+    componentDidUpdate() {
+        console.log("7--componentWillUpdate----组件更新完成");
     }
     // 组件第一次存在于Dom中,函数不会执行
-    // 如果已经存在于DOM中,函数才会执行
-    UNSAFE_componentWillReceiveProps() {
-        console.log("child--componentWillReceiveProps");
+    // 如果已经存在于DOM中,函数才会执行 父组件传递的属性值变化时此钩子函数运行
+    componentWillReceiveProps() {
+        console.log("4-child--componentWillReceiveProps");
     }
-    UNSAFE_componentWillUnmount() {
-        console.log("7--componentWillUnmount--组件卸载时执行");
+    componentWillUnmount() {
+        console.log("8--componentWillUnmount--组件卸载时执行");
     }
     render() {
-        console.log("2--render----组件挂载中");
+        console.log("2--render----组件渲染中");
         return (
             <div>
                 <h2>React生命周期</h2>
